d49c4a1aeb9014565d99f0097d4f16ea18ff3cc0
huachaozhu <huachaozhu2000@gmail.com>
2026-07-03 18:55:58 +0800

    Prune upstream release clutter

--- run.sh
+++
@@ -1,2 +0,0 @@
-nohup bash ./tools/dist_train.sh configs/detseg/grounding_dino_bbyy_swin-b_finetune_obj365.py 4 > nohup_logs/grounding_dino_bbyy_swin-b_finetune_obj365_2.log 2>&1 &
-# nohup bash ./tools/dist_train.sh configs/detseg/grounding_dino_bbyy_swin-b_seg_cityscapes.py 4 > nohup_logs/grounding_dino_bbyy_swin-b_seg_cityscapes.log 2>&1 &
